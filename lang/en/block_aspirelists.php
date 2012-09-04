2175274a9f08ab9312f9e2dfd0c5ce2d0d275408
Jody Kemsley <jwk8@kent.ac.uk>
2012-09-04 16:57:55 +0100

    Adding the moodle 2 block with kent modifications

---
+++ lang/en/block_aspirelists.php
@@ -0,0 +1,26 @@
+<?php
+// Copyright (c) Talis Education Limited, 2011
+// Released under the LGPL Licence - http://www.gnu.org/licenses/lgpl.html. Anyone is free to change or redistribute this code.
+
+$string['pluginname'] = 'Talis Aspire Resource Lists';
+$string['aspirelists'] = 'Resource Lists';
+$string['no_resource_lists_msg'] = 'No resource lists found for';
+
+$string['config_targetAspire'] = 'Target Aspire URL';
+$string['config_targetAspire_desc'] = 'Enter the target Talis Aspire Campus Edition base URL. IMPORTANT: Do not add a trailing slash to the URL.';
+$string['config_targetAspire_default'] = 'Default: http://demo.talisaspire.com';
+$string['config_targetAspire_ex'] = 'http://demo.talisaspire.com';
+$string['config_kg'] = 'Target knowledge group';
+$string['config_kg_desc'] = 'Choose target knowledge grouping level you want to link at, e.g. course or module. This terminology should match that implemented in your Talis Aspire hierarchy.';
+$string['config_kg_ex'] = 'Default: modules';
+$string['config_timePeriod'] = 'Display time period';
+$string['config_timePeriod_desc'] = 'Enter the time period you want to display (1 = 2011/2012, 2 = 2012/2013, etc)';
+$string['config_timePeriod_ex'] = '2';
+
+$string['modules'] = 'Modules';
+$string['courses'] = 'Courses';
+$string['units'] = 'Units';
+$string['programmes'] = 'Programmes';
+$string['subjects'] = 'Subjects';
+
+?>
