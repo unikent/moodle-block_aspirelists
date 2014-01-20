cad5f710013256be946d13be6ecc916f03eb9c03
Skylar Kelty <S.Kelty@kent.ac.uk>
2014-01-20 09:22:59 +0000

    Update ajax.php

--- ajax.php
+++ ajax.php
@@ -13,6 +13,6 @@ $shortname = required_param('shortname', PARAM_RAW);
 $content = \mod_aspirelists\core\aspirelists::get_block_content($id, $shortname);
 
 echo json_encode(array(
-    "text" => "Reading Lists is currently unavaliable due to a system error.",//$content->text,
+    "text" => $content->text,
     "footer" => $content->footer
 ));
