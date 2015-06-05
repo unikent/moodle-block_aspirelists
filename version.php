52fc9614636b4f26a9a3e3047758c71e8ff2973f
Skylar Kelty <S.Kelty@kent.ac.uk>
2015-06-05 11:04:53 +0100

    Update version.php

--- version.php
+++ version.php
@@ -23,6 +23,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015030200;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2015060500;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2013110500;        // Requires this Moodle version.
 $plugin->component = 'block_aspirelists';
